bc52e912e195c6319c168b0dee0f1082c95453e3
Tauqeer Saleem <47822544+TauqeerSaleem@users.noreply.github.com>
2025-03-24 04:12:42 -0400

    Update manim_runner.py

--- manim_runner.py
+++ manim_runner.py
@@ -4,6 +4,15 @@ class ManimRunner:
 
     def start_canvas(self):
         self.terminal.process.start("bash", ["-i"])
+
+        # The following line sets the environment
+        # Make sure in the final version user can set it up themselves
+        # Or installation has option to set a default environment automatically
         self.terminal.process.write("pyenv shell miniforge3-24.11.2-1/envs/manim_env\n".encode())
+
+        # Starts the canvas
         self.terminal.process.write("manim -ql -p --renderer=opengl Canvas.py Canvas\n".encode())
+
+        # Import libararies
+        self.terminal.process.write(("import pandas as pd\n").encode())
 
